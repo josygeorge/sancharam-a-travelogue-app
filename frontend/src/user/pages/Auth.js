eea2cac34a67b1a57889c5a63575fcd49ab8ba29
Josy George <joe.george.josy@gmail.com>
2023-01-11 02:58:30 -0500

    client-user-auth-component

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 const Auth = () => {
+    const auth = useContext(AuthContext);
+    const [isLoginMode, setIsLoginMode] = useState(true);
     return (
-        <div>Auth</div>
+        <Card className="authentication">
+            <h2>Login Required</h2>
+            <hr />
+            <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && (
+                    <Input
+                        element="input"
+                        id="name"
+                        type="text"
+                        label="Your Name"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter a name."
+                        onInput={inputHandler}
+                    />
+                )}
+                <Input
+                    element="input"
+                    id="email"
+                    type="email"
+                    label="E-Mail"
+                    validators={[VALIDATOR_EMAIL()]}
+                    errorText="Please enter a valid email address."
+                    onInput={inputHandler}
+                />
+                <Input
+                    element="input"
+                    id="password"
+                    type="password"
+                    label="Password"
+                    validators={[VALIDATOR_MINLENGTH(5)]}
+                    errorText="Please enter a valid password, at least 5 characters."
+                    onInput={inputHandler}
+                />
+                <Button type="submit" disabled={!formState.isValid}>
+                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                </Button>
+            </form>
+            <Button inverse onClick={switchModeHandler}>
+                SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
+            </Button>
+        </Card>
     )
 }
 
