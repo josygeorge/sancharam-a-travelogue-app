85da8d738b70f8ffc699950c08f8e9983c67333e
Josy George <joe.george.josy@gmail.com>
2023-02-10 19:06:15 -0500

    client-navigation-testing

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -18,6 +18,13 @@ const Users = () => {
                 'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
             places: 2
         },
+        {
+            id: 'u3',
+            name: 'Ron Chard',
+            image:
+                'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            places: 3
+        },
 
     ];
 
