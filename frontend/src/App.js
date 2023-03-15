6d70ddc6b242ee2578c75b6b6ab14340536bbb98
Josy George <joe.george.josy@gmail.com>
2023-03-14 23:19:26 -0400

    client-integration-App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
+import React, { useCallback, useState } from 'react'
 
-function App() {
+import { Switch, Route } from 'react-router-dom'
+import Users from './user/pages/Users';
+
+//
+//
+//
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  //The React useCallback Hook returns a memoized callback function.
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path='/' exact><Users /></Route>
+
+      </Switch>
+    )
+  }
+  //
+  //
   return (
-    <div className="App">
-      Travelogue
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
