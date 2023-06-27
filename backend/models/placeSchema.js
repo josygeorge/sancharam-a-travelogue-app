96726b1d3b996cff827cd18cd0151b830405b9fd
Josy George <joe.george.josy@gmail.com>
2023-06-27 01:12:15 -0400

    place-schema-mongoose-done-backend

--- backend/models/placeSchema.js
+++ backend/models/placeSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
-/* const placeSchema = new Schema({
+const placeSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
@@ -13,6 +13,6 @@ const Schema = mongoose.Schema;
         lng: { type: Number, required: true },
     },
     creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
-}); */
+});
 
 module.exports = mongoose.model('Place', placeSchema);
