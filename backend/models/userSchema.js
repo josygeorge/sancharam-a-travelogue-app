593ebabb40e82644bc4472b82caa8e710d44dd9f
Josy George <joe.george.josy@gmail.com>
2023-06-29 01:23:21 -0400

    user-schema-implementing-mongoose-unique-validator-backend

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
-
+// Schema
 const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -10,5 +11,7 @@ const userSchema = new Schema({
     image: { type: String, required: true },
     places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
 });
+
+//the unique validator will check for duplicate database entries and report them just like any other validation error.
 
 module.exports = mongoose.model('User', userSchema);
