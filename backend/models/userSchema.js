b6a516686ee4da5760b80f9705b3547eb6ea8422
Josy George <joe.george.josy@gmail.com>
2023-06-29 01:27:19 -0400

    user-schema-implementing-mongoose-unique-validator-explanation-backend

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -12,6 +12,26 @@ const userSchema = new Schema({
     places: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Place' }]
 });
 
-//the unique validator will check for duplicate database entries and report them just like any other validation error.
+// the unique validator will check for duplicate database entries and report them just like any other validation error.
+/* 
+Example:
+var user = new User({ username: 'JohnSmith', email: 'john.smith@gmail.com', password: 'j0hnNYb0i' });
+user.save(function (err) {
+    console.log(err);
+});
+{
+    message: 'Validation failed',
+    name: 'ValidationError',
+    errors: {
+        username: {
+            message: 'Error, expected `username` to be unique. Value: `JohnSmith`',
+            name: 'ValidatorError',
+            kind: 'unique',
+            path: 'username',
+            value: 'JohnSmith'
+        }
+    }
+}
+*/
 
 module.exports = mongoose.model('User', userSchema);
